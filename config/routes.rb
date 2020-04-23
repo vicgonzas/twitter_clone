e22359df42c26d4c8da9438d33ff2ebf86e2e5e4
Victor <s.gonzalez.victor@gmail.com>
2020-04-23 01:09:37 -0400

    Designing our Tweet Controller/Generating Views/Creating an Index page/Adding Authentication/Setting up Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+ root 'tweets#index'
+ 
+ devise_for :users
+
+ resources :users do 
+    resources :tweets
+ end 
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
